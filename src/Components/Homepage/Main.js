437501f3a9c3f4a40b70fcc059e04114383f5462
unknown <nedelusandrei@gmail.com>
2018-11-23 19:30:58 +0200

    added header menu

--- src/Components/Homepage/Main.js
+++ src/Components/Homepage/Main.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
 import Header from "../Header";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
+firebase.initializeApp({
+    apiKey: "AIzaSyCboc020iBf36hc1CeOt5Uf3r5aawnz5vI",
+    authDomain: "itec-2018-343d8.firebaseapp.com"
+});
 class Main extends Component {
+    state = {
+        isSingedIn: false
+    };
+    uiConfig = {
+        signInFlow: "popup",
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        callbacks: {
+            signInSuccess: () => false
+        }
+    };
+
+    componentDidMount = () => {
+        firebase.auth().onAuthStateChanged(user => {
+            this.setState({ isSignedIn: !!user });
+            console.log("user", user);
+        });
+    };
+
     navMenu = () => {
         const x = document.getElementById("myTopnav");
         if (x.className === "topnav") {
@@ -10,32 +38,60 @@ class Main extends Component {
             x.className = "topnav";
         }
     };
-    hamburger = x => {
-        x.classList.toggle("change");
-    };
+
     render() {
         return (
             <div>
-                <Header>
-                    <div className="logo">QuizMe!</div>
-                    <div className="topnav" id="myTopnav">
-                        <a href="#" className="active">
-                            Events
-                        </a>
-                        <a href="#news">News</a>
-                        <a href="#contact">Contact</a>
-                        <a href="#about">About</a>
-                        <a
-                            href="javascript:void(0);"
-                            className="icon"
-                            onClick={this.navMenu}
-                        >
-                            <div className="hamburger" />
-                            <div className="hamburger" />
-                            <div className="hamburger" />
-                        </a>
+                {this.state.isSignedIn ? (
+                    <div>
+                        <Header>
+                            <div className="topnav" id="myTopnav">
+                                <div className="logo">QuizMe!</div>
+                                <a
+                                    href="javascript:void(0);"
+                                    className="icon"
+                                    style={{ float: "right" }}
+                                    onClick={this.navMenu}
+                                >
+                                    <div className="hamburger" />
+                                    <div className="hamburger" />
+                                    <div className="hamburger" />
+                                </a>
+                                <div className="buttons">
+                                    <a style={{ display: "none" }} />
+                                    <a className="userName">
+                                        {
+                                            firebase.auth().currentUser
+                                                .displayName
+                                        }
+                                    </a>
+                                    <a className="hover" href="#">
+                                        Events
+                                    </a>
+                                    <a className="hover" href="#news">
+                                        Quiz
+                                    </a>
+
+                                    <a
+                                        className="hover"
+                                        onClick={() =>
+                                            firebase.auth().signOut()
+                                        }
+                                    >
+                                        Sign out!
+                                    </a>
+                                </div>
+                            </div>
+                        </Header>
                     </div>
-                </Header>
+                ) : (
+                    <div className="bg">
+                        <StyledFirebaseAuth
+                            uiConfig={this.uiConfig}
+                            firebaseAuth={firebase.auth()}
+                        />
+                    </div>
+                )}
             </div>
         );
     }
